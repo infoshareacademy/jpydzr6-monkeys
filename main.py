8a442db82e796b8d32154e41bceef262715d831b
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2024-10-27 20:09:17 +0100

    Function choose_action has been added

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from enum import Enum
 
 class Actions(Enum):
-    USER = 'Zarządzanie użytkownikiem'
-    ACCOUNT = 'Zarządzanie kontami bankowymi'
-    TRANSACTIONS = 'Zarządzanie transakcjami'
-    DEBTS = 'Zarządzanie zadłużeniem'
-    REPORT = 'Wyświetl raport'
-    EXIT = 'Wyjście z programu'
+    U = 'Zarządzanie użytkownikiem'
+    K = 'Zarządzanie kontami bankowymi'
+    T = 'Zarządzanie transakcjami'
+    Z = 'Zarządzanie zadłużeniem'
+    R = 'Wyświetl raport'
+    Q = 'Wyjście z programu'
+
+
+def choose_action():
+    while True:
+        print('Wybierz akcję do wykonania: \n')
+        for action in Actions:
+            print(f'{action.value} - {action.name}')
+        choice = input().upper()
+
+        try:
+            action = Actions[choice]
+        except KeyError:
+            print('Podano nieprawidłową wartość.\n')
+            continue
+
+        print(action)
+        return action
+
 
 def main():
-    pass
+    print('Witaj w programie do budżetowania.\n')
+    choose_action()
 
 if __name__ == '__main__':
     main()
