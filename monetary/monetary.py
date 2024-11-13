3fd3bf90b35893da5a704f2f5485a43c7bbc153c
marekwocka <marekwocka@gmail.com>
2024-11-14 00:21:13 +0100

    Added static method for conversion from major monetary unit to its minors

--- monetary/monetary.py
+++ monetary/monetary.py
@@ -1,7 +1,11 @@
+from math import floor
+
 from currency import Currency
+
 
 class Monetary:
     __slot__ = ("__amount", "__currency")
+
     def __init__(self, amount: int, currency: Currency) -> None:
         try:
             self.__validate_amount(amount)
@@ -14,7 +18,35 @@ class Monetary:
     @staticmethod
     def __validate_amount(amount) -> bool:
         if not isinstance(amount, int):
-            raise TypeError("Amount must be an integer"),
+            raise TypeError("Amount must be an integer")
         if amount < 0:
             raise ValueError("Amount can not be negative")
         return True
+
+    @staticmethod
+    def major_to_minor_monetary_unit(major_value: int | float | str, currency: Currency) -> int:
+        factor = pow(currency.get("base"), currency.get("exponent"))
+        match major_value:
+            case int():
+                if major_value < 0:
+                    raise ValueError("Amount can not be negative")
+                else:
+                    return major_value * factor
+            case float():
+                if major_value < 0.0:
+                    raise ValueError("Amount can not be negative")
+                else:
+                    return floor(major_value * factor)
+            case str():
+                major_value.strip().replace(",", ".")
+                try:
+                    converted = float(major_value)
+                except:
+                    raise ValueError("Given string cannot be converted into float")
+                else:
+                    if converted < 0.0:
+                        raise ValueError("Amount can not be negative")
+                    else:
+                        return floor(converted * factor)
+            case _:
+                raise TypeError("Wrong type of given value")
