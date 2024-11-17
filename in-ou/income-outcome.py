c8a3b1d446428b5af2c1908aa453d6a67e7a2aa2
Dmarzorro <jaroslawmazur63@gmail.com>
2024-11-17 15:33:50 +0100

    added load_budget_from_file method

--- in-ou/income-outcome.py
+++ in-ou/income-outcome.py
@@ -14,3 +14,18 @@ class BudgetManager:
             print("Budzet zostal zapisany do pliku.")
         except IOError:
             print("Blad: Nie udalo sie zapisac budzetu do pliku")
+
+    def load_budget_from_file(self):
+        try:
+            with open(self.file_name, 'r', encoding='utf-8') as file:
+                self.budget = json.load(file)
+        except FileNotFoundError:
+            print("Nie znaleziono pliku budżetowego.")
+        except json.decoder.JSONDecodeError:
+            print("Plik budżetu jest uszkodzony.")
+        except IOError:
+            print("Blad: Nie udalo sie wczytac pliku budzetu.")
+
+    def add_budget_entry(self, entry_type, amount, description):
+        if not isinstance(amount, (int, float)) or amount <= 0:
+            print("Blad: kwota musi byc liczba dodatnia.")
