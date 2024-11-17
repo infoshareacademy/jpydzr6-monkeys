ff53931ce284200b1f853562a5cd3bb46f405e50
Dmarzorro <jaroslawmazur63@gmail.com>
2024-11-17 20:23:39 +0100

    added initialize_databes function

--- in-ou/income-outcome.py
+++ in-ou/income-outcome.py
@@ -4,9 +4,9 @@ from datetime import datetime
 class BudgetManager:
     def __init__(self, db_name='budget.db'):
         self.db_name = db_name
-        self.create_tables()
+        self.initialize_database()
 
-    def create_connectiion(self):
+    def create_connection(self):
         return sqlite3.connect(self.db_name) #połączenie z bazą dannych sql
 
     def create_tables(self):
@@ -21,6 +21,21 @@ class BudgetManager:
                 category TEXT,
                 date TEXT NOT NULL
             )
+            ''')
+            conn.commit()
+
+    def initialize_database(self):
+        with self.create_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS budget (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    entry_type TEXT NOT NULL,
+                    amount REAL NOT NULL,
+                    description TEXT,
+                    category TEXT,
+                    date TEXT NOT NULL
+                )
             ''')
             conn.commit()
 
@@ -53,6 +68,25 @@ class BudgetManager:
             for row in rows:
                 print(f"{row[0]}. {row[1]}: {row[2]} PLN, {row[3]} (Kategoria: {row[4]}, Data: {row[5]})")
         # status konta ( wydatki, przychody, saldo )
+    def show_budget_summary(self):
+        try:
+            with self.create_connection() as conn:
+                cursor = conn.cursor()
+
+                cursor.execute("SELECT SUM(amount) FROM budget WHERE entry_type = 'income'")
+                income = cursor.fetchone()[0] or 0
+
+                cursor.execute("SELECT SUM(amount) FROM budget WHERE entry_type = 'outcome'")
+                outcome = cursor.fetchone()[0] or 0
+
+                balance = income - outcome
+
+                print(
+                    f"Podsumowanie budżetu:\n - Dochody: {income:.2f} PLN\n - Wydatki: {outcome:.2f} PLN\n - Saldo: {balance:.2f} PLN")
+        except sqlite3.Error as e:
+            print(f"Błąd bazy danych: {e}")
+        except Exception as e:
+            print(f"Nieoczekiwany błąd: {e}")
 
     def delete_budget_entry(self, entry_id):
         with self.create_connection() as conn:
