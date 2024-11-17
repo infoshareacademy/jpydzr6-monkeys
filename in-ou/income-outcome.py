9b29e5bd2f4ff7ab9313b8bdf29a63390a0783ca
Dmarzorro <jaroslawmazur63@gmail.com>
2024-11-17 15:29:27 +0100

    added save_budget_to_file method

--- in-ou/income-outcome.py
+++ in-ou/income-outcome.py
@@ -6,3 +6,11 @@ class BudgetManager:
         self.budget = []
         self.file_name = file_name
         self.load_budget_from_file()
+
+    def save_budget_to_file(self):
+        try:
+            with open(self.file_name, 'w', encoding='utf-8') as file:
+                json.dump(self.budget, file, ensure_ascii=False, indent=4)
+            print("Budzet zostal zapisany do pliku.")
+        except IOError:
+            print("Blad: Nie udalo sie zapisac budzetu do pliku")
