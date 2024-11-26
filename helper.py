626afeee3f1e55dffd1e9b01be34d0b10c10aa7f
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2024-11-26 17:45:22 +0100

    parameter error_message has been added to methods

--- helper.py
+++ helper.py
@@ -7,15 +7,15 @@ class InvalidData(Exception):
 
 class Helper:
     @staticmethod
-    def check_value(data: Any, test_value: Callable) -> Any:
+    def check_value(data: Any, test_value: Callable, error_message: str) -> Any:
         try:
             validated_data = test_value(data)
             return validated_data
         except ValueError:
-            raise InvalidData('Nieprawidłowe dane: ') from None
+            raise InvalidData(error_message) from None
 
     @staticmethod
-    def check_length(data: Any, length: int) -> Any:
+    def check_length(data: Any, length: int, error_message: str) -> Any:
         if len(data) == length:
             return data
-        raise InvalidData('Nieprawidłowe dane') from None
+        raise InvalidData(error_message) from None
