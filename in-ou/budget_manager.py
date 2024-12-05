dc0521bec36843dfa0b3c9685be2123ba5929a82
Marek Wocka <marekwocka@gmail.com>
2024-12-05 23:53:49 +0100

    Update with Monetary class

--- in-ou/budget_manager.py
+++ in-ou/budget_manager.py
@@ -1,13 +1,16 @@
 import sqlite3
 from datetime import datetime
+from money import Monetary, Currency
+
 
 class BudgetManager:
-    def __init__(self, db_name='budget.db'):
+    def __init__(self, currency: Currency, db_name='budget.db'):
         self.db_name = db_name
         self.budget = []
         self.create_table()
         self.load_budget_from_file()
         self.__entry_types = ('income', 'outcome')
+        self.__currency = currency
 
     def create_connection(self):
         return sqlite3.connect(self.db_name)
@@ -55,27 +58,30 @@ class BudgetManager:
             } for row in rows]
         print("Budżet został załadowany z bazy danych.")
 
-    def add_budget_entry(self, entry_type, amount, description, category="brak kategorii"):
+    def add_budget_entry(self, entry_type: str, entry: Monetary, description: str, category: str="brak kategorii") -> None:
         if entry_type not in self.__entry_types:
             print("Blad: Nieprawidłowy rodzaj wpisu. Wybierz 'income' lub 'outcome'.") #Zmienić na I / O, czy zostawić pełne słowa?
             return
-        if not isinstance(amount, (int, float)) or amount <= 0:
-            print("Blad: Kwota musi być liczbą dodatnią.")
+        if not isinstance(entry, Monetary):
+            print("Blad: Wpis jest złego typu danych")
+            return
+        if entry.amount < 0:
+            print("Blad: Kwota musi być liczbą nieujemną.")
             return
         if len(description) > 255:
             print("Blad: Opis jest za długi (maksymalnie 255 znaków).")
             return
 
-        entry = {
+        record = {
             "type": entry_type,
-            "amount": amount,
+            "amount": entry.amount,
             "description": description,
             "category": category,
             "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         }
-        self.budget.append(entry)
+        self.budget.append(record)
         self.save_budget_to_file()
-        print(f"Pomyślnie dodano wpis: {entry_type} - {amount} PLN, opis: {description}, kategoria: {category}")
+        print(f"Pomyślnie dodano wpis: {entry_type} - {entry}, opis: {description}, kategoria: {category}")
 
     def add_budget_entry_input(self): #Dodawanie wpisów z inputem, również do usunięcia w przyszłości.
         while True:
@@ -107,6 +113,8 @@ class BudgetManager:
         if not category:
             category = "Brak kategorii" # domyśla kategoria
 
+        amount = Monetary.major_to_minor_unit(amount, self.__currency)
+        amount = Monetary(amount, self.__currency)
         self.add_budget_entry(entry_type, amount, description, category)
 
     def show_budget(self):
@@ -116,7 +124,8 @@ class BudgetManager:
             sorted_budget = sorted(self.budget, key=lambda x: x['date'])
             for i, entry in enumerate(sorted_budget, 1):
                 category = entry.get('category', 'Brak kategorii')
-                print(f"{i}. {entry['type']}: {entry['amount']} PLN, {entry['description']} "
+                amount = Monetary(entry['amount'], self.__currency)
+                print(f"{i}. {entry['type']}: {amount}, {entry['description']} "
                       f"(Kategoria: {category}, Data: {entry['date']})")
         # status konta ( wydatki, przychody, saldo )
     def show_budget_summary(self):
@@ -124,13 +133,15 @@ class BudgetManager:
             print("Brak danych do podsumowania.")
             return
         try:
-            income = sum(entry['amount'] for entry in self.budget if entry['type'] == 'income')
+            incomes = sum(entry['amount'] for entry in self.budget if entry['type'] == 'income')
             expenses = sum(entry['amount'] for entry in self.budget if entry['type'] == 'outcome')
-            balance = income - expenses
+            incomes = Monetary(incomes, self.__currency)
+            expenses = Monetary(expenses, self.__currency)
+            balance = incomes - expenses
             print("Podsumowanie budżetu:")
-            print(f" - Dochody: {income:.2f} PLN")
-            print(f" - Wydatki: {expenses:.2f} PLN")
-            print(f" - Saldo: {balance:.2f} PLN")
+            print(f" - Dochody: {incomes}")
+            print(f" - Wydatki: {expenses}")
+            print(f" - Saldo: {balance}")
         except KeyError as e:
             print(f"Blad: brakuje klucza w danych wpisu budzetowego ({e})")
     #filtracja TYLKO przychodów zamiast ogólnych wpisów
@@ -143,7 +154,8 @@ class BudgetManager:
                 return
             print(f"Lista dochodów w kategorii '{category}':")
             for i, entry in enumerate(incomes, 1):
-                print(f"{i}. Kwota: {entry['amount']} PLN, Opis: {entry['description']}, Data: {entry['date']}")
+                entry_monetary = Monetary(entry['amount'], self.__currency)
+                print(f"{i}. Kwota: {entry_monetary}, Opis: {entry['description']}, Data: {entry['date']}")
         except KeyError as e:
             print(f"Błąd: Brakuje klucza w danych budżetu ({e}).")
         except Exception as e:
@@ -158,7 +170,8 @@ class BudgetManager:
                 return
             print(f"Lista wydatków w kategorii '{category}:")
             for i, entry in enumerate(outcomes, 1):
-                print(f"{i}. Kwota: {entry['amount']} PLN, Opis: {entry['description']}, Data: {entry['date']}")
+                entry_monetary = Monetary(entry['amount'], self.__currency)
+                print(f"{i}. Kwota: {entry_monetary}, Opis: {entry['description']}, Data: {entry['date']}")
         except KeyError as e:
             print(f"Błąd: Brakuje klucza w danych budżetu ({e}).")
         except Exception as e:
@@ -173,7 +186,8 @@ class BudgetManager:
                 return
             print("Lista dochodów: ")
             for i, entry in enumerate(incomes, 1):
-                print(f"{i}. Kwota: {entry['amount']} PLN, Opis: {entry['description']}, Kategoria: "
+                entry_monetary = Monetary(entry['amount'], self.__currency)
+                print(f"{i}. Kwota: {entry_monetary}, Opis: {entry['description']}, Kategoria: "
                       f"{entry.get('category', 'Brak kategorii')}, Data: {entry['date']}")
         except KeyError as e:
             print(f"Błąd: Brakuje klucza w danych budżetu ({e}). ")
@@ -188,7 +202,8 @@ class BudgetManager:
                 return
             print("Lista wydatków: ")
             for i, entry in enumerate(outcomes, 1):
-                print(f"{i}. Kwota: {entry['amount']} PLN, Opis: {entry['description']}, "
+                entry_monetary = Monetary(entry['amount'], self.__currency)
+                print(f"{i}. Kwota: {entry_monetary}, Opis: {entry['description']}, "
                       f"Kategoria: {entry.get('category', 'Brak kategorii')}, Data: {entry['date']}")
         except KeyError as e:
             print(f"Błąd: Brakuje klucza w danych budżetu ({e}).")
@@ -198,7 +213,8 @@ class BudgetManager:
     def edit_budget_entry(self, index):
         try:
             entry = self.budget[index - 1]
-            print(f"Edycja wpisu: {entry['type']}: {entry['amount']} PLN, {entry['description']}")
+            entry_monetary = Monetary(entry['amount'], self.__currency)
+            print(f"Edycja wpisu: {entry['type']}: {entry_monetary}, {entry['description']}")
 
             #Tu użytkownik wpisuje nowe dane, Value Error wystarczy czy coś więcej?
             new_type = input("Nowy typ (income/outcome): ").strip().lower()
@@ -208,7 +224,9 @@ class BudgetManager:
             try:
                 new_amount = input("Nowa kwota: ").strip()
                 if new_amount:
-                    entry["amount"] = float(new_amount)
+                    new_amount = float(new_amount)
+                    new_amount = Monetary.major_to_minor_unit(new_amount, self.__currency)
+                    new_amount = Monetary(new_amount, self.__currency)
             except ValueError:
                 print("Błąd: niepoprawna kwota. Pozostawiono starą wartość.")
 
@@ -218,7 +236,7 @@ class BudgetManager:
             # Pokazanie zmian przed zapisaniem
             print("\nProponowane zmiany:")
             print(f" - Typ: {entry['type']}")
-            print(f" - Kwota: {entry['amount']:.2f} PLN")
+            print(f" - Kwota: {new_amount}")
             print(f" - Opis: {entry['description']}")
             print(f" - Kategoria: {entry['category']}")
 
@@ -227,7 +245,7 @@ class BudgetManager:
             if confirm != "tak":
                 print("Edycja anulowana.")
                 return
-
+            entry["amount"] = new_amount.amount
             # Aktualizacja daty i zapisanie zmian
             entry["date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             self.save_budget_to_file()
@@ -242,7 +260,8 @@ class BudgetManager:
         try:
             print(f"Próba usunięcia wpisu o indeksie: {index}")
             entry = self.budget.pop(index - 1)
+            entry_monetary = Monetary(entry['amount'], self.__currency)
             self.save_budget_to_file()
-            print(f"Wpis usunięty: {entry['type']} - {entry['amount']} PLN, {entry['description']}")
+            print(f"Wpis usunięty: {entry['type']} - {entry_monetary}, {entry['description']}")
         except IndexError:
             print(f"Nie ma wpisu z podanym indeksem: {index}")
