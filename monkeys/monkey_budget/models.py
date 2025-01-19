a6d577464c8bd9fe5fea9cc44c1a46a2f331e66b
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-01-19 19:11:23 +0100

    Money acoount model has been made with migration

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models import BigIntegerField
+
 
 # Create your models here.
+
+
+class MoneyAccount(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    name = models.CharField(max_length=100) # krótka nazwa konta nadawana przez użytkownika np. 'Oszczędnościowe mbank'
+    balance = BigIntegerField()
+    currency_id = models.CharField(max_length=3) #ISO4217 np. PLN, USD
+    description = models.CharField(max_length=512) # dłuższy opis konta dodawany przez użytkownika
+    number = models.CharField(max_length=50, default=None, null=True) # numer konta dodawany przez użytkownika,
+                                                                    # w zamyśle pełen numer konta bankowego lub
+                                                                    # brak np. dla gotówki czy bonów
+
+    class Meta:
+        app_label = 'monkey_budget'
+        verbose_name = 'Money Account'
