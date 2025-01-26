d469f069aae0e45b49ab70e98f8c1ac2d85764be
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-01-26 19:53:02 +0100

    Lists of choices in MoneyAccount have been corrected. MoneyAccount model has been migrated

--- monkeys/monkey_budget/admin.py
+++ monkeys/monkey_budget/admin.py
@@ -3,4 +3,3 @@ from .models import MoneyAccount
 
 # Register your models here.
 admin.site.register(MoneyAccount)
-
--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -11,9 +11,9 @@ class MoneyAccount(models.Model):
     user_id = models.ForeignKey(User, related_name='money_accounts', on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=100) # krótka nazwa konta nadawana przez użytkownika np. 'Oszczędnościowe mbank'
     balance = BigIntegerField()
-    types = ['konto bankowe', 'gotówka', 'bony', 'inne'] # typy przechowywania pieniędzy do wybrania przez użytkownika
+    types = [('BA', 'konto bankowe'), ('CA', 'gotówka'), ('CO', 'bony'), ('AN', 'inne')] # typy przechowywania pieniędzy do wybrania przez użytkownika
     type = models.CharField(choices=types, max_length=50, default='konto bankowe') # typ konta do wybrania z pośród podanych kategorii
-    currency_code = ['PLN', 'USD', 'EUR']
+    currency_code = [('PLN', 'złotówki') , ('USD', 'dolary'), ('EUR', 'euro')]
     currency_code = models.CharField(choices=currency_code, max_length=3, default='PLN') #ISO4217 np. PLN, USD
     description = models.TextField(max_length=512) # dłuższy opis konta dodawany przez użytkownika
     number = models.CharField(max_length=50, default=None, null=True) # numer konta dodawany przez użytkownika,
