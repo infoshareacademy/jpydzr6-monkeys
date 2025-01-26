cda0d057b65bebc42674a5ba3dc508bf4dea5a81
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-01-26 19:31:50 +0100

    MoneyAccount model has been recreated and migrations connected with it has been deleted. File requirements.txt has been updated

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import BigIntegerField
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -7,14 +8,17 @@ from django.db.models import BigIntegerField
 
 class MoneyAccount(models.Model):
     id = models.AutoField(primary_key=True, null=False)
+    user_id = models.ForeignKey(User, related_name='money_accounts', on_delete=models.CASCADE, null=False)
     name = models.CharField(max_length=100) # krótka nazwa konta nadawana przez użytkownika np. 'Oszczędnościowe mbank'
     balance = BigIntegerField()
-    currency_id = models.CharField(max_length=3) #ISO4217 np. PLN, USD
-    description = models.CharField(max_length=512) # dłuższy opis konta dodawany przez użytkownika
+    types = ['konto bankowe', 'gotówka', 'bony', 'inne'] # typy przechowywania pieniędzy do wybrania przez użytkownika
+    type = models.CharField(choices=types, max_length=50, default='konto bankowe') # typ konta do wybrania z pośród podanych kategorii
+    currency_code = ['PLN', 'USD', 'EUR']
+    currency_code = models.CharField(choices=currency_code, max_length=3, default='PLN') #ISO4217 np. PLN, USD
+    description = models.TextField(max_length=512) # dłuższy opis konta dodawany przez użytkownika
     number = models.CharField(max_length=50, default=None, null=True) # numer konta dodawany przez użytkownika,
                                                                     # w zamyśle pełen numer konta bankowego lub
                                                                     # brak np. dla gotówki czy bonów
-
     class Meta:
         app_label = 'monkey_budget'
         verbose_name = 'Money Account'
