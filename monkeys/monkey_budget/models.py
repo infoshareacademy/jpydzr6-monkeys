69475ecf7e2bb7414789a462c7daffc8134f281c
Dorota Grześkowiak <109593910+ilsalunte@users.noreply.github.com>
2025-01-28 13:34:15 +0100

    feat: Create MoneyAccount model with data type tests (#21)
    * Money acoount model has been made with migration
    
    * MoneyAccount model has been recreated and migrations connected with it has been deleted. File requirements.txt has been updated
    
    * Lists of choices in MoneyAccount have been corrected. MoneyAccount model has been migrated
    
    * list of choice of currency has been renemed to currency_codes
    
    * pytest.ini file has been added. Tests of data type in MoneyAccount model have been created
    
    * Atribute name has been removed from MoneyAccount. __str__ method has been added.

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models import BigIntegerField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class MoneyAccount(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    user_id = models.ForeignKey(User, related_name='money_accounts', on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=100) # krótka nazwa konta nadawana przez użytkownika np. 'Oszczędnościowe mbank'
+    balance = BigIntegerField()
+    types = [('BA', 'konto bankowe'), ('CA', 'gotówka'), ('CO', 'bony'), ('AN', 'inne')] # typy przechowywania pieniędzy do wybrania przez użytkownika
+    type = models.CharField(choices=types, max_length=50, default='konto bankowe') # typ konta do wybrania z pośród podanych kategorii
+    currency_codes = [('PLN', 'złotówki') , ('USD', 'dolary'), ('EUR', 'euro')]
+    currency_code = models.CharField(choices=currency_codes, max_length=3, default='PLN') #ISO4217 np. PLN, USD
+    description = models.TextField(max_length=512) # dłuższy opis konta dodawany przez użytkownika
+
+    def __str__(self):
+        return f'{self.name}: {self.balance} {self.currency_code}'
+
+
+    class Meta:
+        app_label = 'monkey_budget'
+        verbose_name = 'Money Account'
