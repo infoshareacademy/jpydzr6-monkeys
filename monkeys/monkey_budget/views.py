642080fb2c50f3ebd2acbb97b6aa9e75349bdba4
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-01-28 14:02:08 +0100

    Basic view of money_accounts has been added

--- monkeys/monkey_budget/views.py
+++ monkeys/monkey_budget/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def money_accounts(request):
+    return HttpResponse("Zarządzanie zasobami pieniężnymi.")
