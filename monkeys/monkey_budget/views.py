9e9db25d5e3a3a6d5aa79c8dc2c17e4221df9a8f
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-02-02 18:55:19 +0100

    Templates for money accounts functionalities have been added to the views

--- monkeys/monkey_budget/views.py
+++ monkeys/monkey_budget/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -7,13 +8,13 @@ def money_accounts(request):
     return HttpResponse('Subemnu zarządzania kontami.')
 
 def add_money_account(request):
-    return HttpResponse('Dodawanie konta')
+    return render(request, 'add_account.html')
 
 def edit_money_account(request):
-    return HttpResponse('Edycja konta')
+    return render(request, 'edit_account.html')
 
 def delete_money_account(request):
-    return HttpResponse('Usuwanie konta')
+    return render(request, 'delete_account.html')
 
 def show_money_account(request):
-    return HttpResponse('Pokaż konta')
+    return render(request, 'add_account.html')
