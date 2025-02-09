63edf60633c166f8f09cf651a3cca69fdad8c686
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-02-09 20:20:08 +0100

    Now accounts are filter by user_id

--- monkeys/monkey_budget/views.py
+++ monkeys/monkey_budget/views.py
@@ -1,25 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import MoneyAccount
 
 
 # Create your views here.
-
+# TODO - brak informacji o id użytkownika, jest wpisane na sztywno
 def money_accounts(request):
-    context = {'accounts': MoneyAccount.objects.all()}
-    return render(request, 'account_menu.html', context)
-
-def add_money_account(request):
-    return render(request, 'add_account.html')
-
-def edit_money_account(request):
-    return render(request, 'edit_account.html')
-
-def delete_money_account(request):
-    return render(request, 'delete_account.html')
+    all_accounts = MoneyAccount.objects.filter(user_id=2)
+    context = {'accounts': all_accounts}
+    return render(request, 'account/account_menu.html', context)
 
 def show_money_account(request, account_id):
-    # context = {'accounts': MoneyAccount.objects.all()}
-    # return render(request, 'show_account.html', context)
-    return HttpResponse(f'Szczegóły konta numer: {account_id}')
+    chosen_account = get_object_or_404(MoneyAccount, pk=account_id)
+    context = {'account': chosen_account}
+    return render(request, 'account/show_account.html', context)
+
+def add_money_account(request):
+    return render(request, 'account/add_account.html')
+
+def edit_money_account(request):
+    return render(request, 'account/edit_account.html')
+
+def delete_money_account(request):
+    return render(request, 'account/delete_account.html')
