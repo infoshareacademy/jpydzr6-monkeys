156b51882685b80ed745e6335692864bd3d6e75b
romcio <roman@szubryt.eu>
2025-02-09 21:06:40 +0100

    add categories & subcategories to data base model

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -24,3 +24,38 @@ class MoneyAccount(models.Model):
     class Meta:
         app_label = 'monkey_budget'
         verbose_name = 'Money Account'
+
+
+class MainCategory(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    user_id = models.ForeignKey(User, related_name='main_categories', on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=100)
+    category_types = [
+        ('INC', 'Przychód'),
+        ('EXP', 'Wydatek'),
+    ]
+    category_type = models.CharField(choices=category_types, max_length=3)
+    description = models.TextField(max_length=512, blank=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.get_category_type_display()})'
+
+    class Meta:
+        app_label = 'monkey_budget'
+        verbose_name = 'Main Category'
+        verbose_name_plural = 'Main Categories'
+
+
+class SubCategory(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    parent_category = models.ForeignKey(MainCategory, related_name='subcategories', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=512, blank=True)
+
+    def __str__(self):
+        return f'{self.parent_category.name} - {self.name}'
+
+    class Meta:
+        app_label = 'monkey_budget'
+        verbose_name = 'Sub Category'
+        verbose_name_plural = 'Sub Categories'
