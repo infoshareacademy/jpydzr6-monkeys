97f392cead4601546fb68c7d4d464ac45ef1b5ae
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-02-20 20:06:09 +0100

    Template of the account form has been modified

--- monkeys/monkey_budget/views.py
+++ monkeys/monkey_budget/views.py
@@ -17,6 +17,7 @@ def show_money_account(request, account_id):
 
 def add_money_account(request):
     all_accounts = MoneyAccount.objects.filter(user_id=2)
+
     context = {'accounts': all_accounts}
     return render(request, 'account/add_account.html', context)
 
