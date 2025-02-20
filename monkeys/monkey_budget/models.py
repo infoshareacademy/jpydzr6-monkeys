b3ac296d4579aa70f63e78b3f6e3ff575875d7dc
Dorota Grześkowiak <109593910+ilsalunte@users.noreply.github.com>
2025-02-20 13:56:08 +0100

    feat: create templates for account (#30)
    * Templates for money accounts functionalities have been created
    
    * Templates for money accounts functionalities have been added to the views
    
    * View for account_menu with link to page with account details have been added
    
    * Modifications of views and urls for accounts have been made
    
    * Method get_type_display_name has been added to MoneyAccount
    
    * File organization has been changed
    
    * Now accounts are filter by user_id
    
    * MoneyAccount model code refactoring has been made. Show account style has been added
    
    * Bootstrap has been installed and added to settings.py
    
    * Offcanvas Sidebar has been added to account display
    
    * Button for deleting account has been added
    
    * Inheritance mechanism has been added
    
    * Some bacground colors have been added

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -1,6 +1,5 @@
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.db.models import BigIntegerField
 from django.contrib.auth.models import User
 import django.utils.timezone
 
@@ -10,16 +9,23 @@ from .money import Monetary, CurrencyHelper
 class MoneyAccount(models.Model):
     id = models.AutoField(primary_key=True, null=False)
     user_id = models.ForeignKey(User, related_name='money_accounts', on_delete=models.CASCADE, null=False)
-    name = models.CharField(max_length=100) # krótka nazwa konta nadawana przez użytkownika np. 'Oszczędnościowe mbank'
-    balance = BigIntegerField()
-    types = [('BA', 'konto bankowe'), ('CA', 'gotówka'), ('CO', 'bony'), ('AN', 'inne')] # typy przechowywania pieniędzy do wybrania przez użytkownika
-    type = models.CharField(choices=types, max_length=50, default='konto bankowe') # typ konta do wybrania z pośród podanych kategorii
-    currency_code = models.CharField(choices=CurrencyHelper.get_currencies_set(), max_length=3, default='PLN') #ISO4217 np. PLN, USD
-    description = models.TextField(max_length=512) # dłuższy opis konta dodawany przez użytkownika
+    # krótka nazwa konta nadawana przez użytkownika np. 'Oszczędnościowe mbank'
+    name = models.CharField(max_length=100)
+    balance = models.BigIntegerField()
+    #  typy przechowywania pieniędzy do wybrania przez użytkownika
+    types = [('BA', 'konto bankowe'), ('CA', 'gotówka'), ('CO', 'bony'), ('AN', 'inne')]
+    # typ konta do wybrania spośród podanych kategorii
+    type = models.CharField(choices=types, max_length=50, default='konto bankowe')
+    # ISO4217 np. PLN, USD
+    currency_code = models.CharField(choices=CurrencyHelper.get_currencies_set(), max_length=3, default='PLN')
+    # dłuższy opis konta dodawany przez użytkownika
+    description = models.TextField(max_length=512)
 
     def __str__(self):
         return f'{self.name}: {self.balance} {CurrencyHelper.get_currency_by_its_code(self.currency_code)["code"]}'
 
+    def get_type_display_name(self):
+        return dict(self.types).get(self.type, self.type)
 
     class Meta:
         app_label = 'monkey_budget'
--- monkeys/monkey_budget/views.py
+++ monkeys/monkey_budget/views.py
@@ -1,19 +1,29 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import MoneyAccount
 
 
 # Create your views here.
-
+# TODO - brak informacji o id użytkownika, jest wpisane na sztywno do zmiany po dodaniu możliwości logowania
 def money_accounts(request):
-    return HttpResponse('Subemnu zarządzania kontami.')
+    all_accounts = MoneyAccount.objects.filter(user_id=2)
+    context = {'accounts': all_accounts}
+    return render(request, 'account/account_menu.html', context)
+
+def show_money_account(request, account_id):
+    chosen_account = get_object_or_404(MoneyAccount, pk=account_id)
+    all_accounts = MoneyAccount.objects.filter(user_id=2)
+    context = {'account': chosen_account, 'accounts': all_accounts}
+    return render(request, 'account/show_account.html', context)
 
 def add_money_account(request):
-    return HttpResponse('Dodawanie konta')
+    all_accounts = MoneyAccount.objects.filter(user_id=2)
+    context = {'accounts': all_accounts}
+    return render(request, 'account/add_account.html', context)
 
 def edit_money_account(request):
-    return HttpResponse('Edycja konta')
+    return render(request, 'account/edit_account.html')
 
-def delete_money_account(request):
-    return HttpResponse('Usuwanie konta')
-
-def show_money_account(request):
-    return HttpResponse('Pokaż konta')
+def delete_money_account(request, account_id):
+    account = get_object_or_404(MoneyAccount, id=account_id)
+    account.delete()
+    return redirect('konta')
