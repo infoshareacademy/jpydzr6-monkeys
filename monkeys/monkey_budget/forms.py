b56f7ec72ae9160fb380bbf8f5d6987daa28ceb3
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-02-21 20:52:33 +0100

    Change form labels to polish

--- monkeys/monkey_budget/forms.py
+++ monkeys/monkey_budget/forms.py
@@ -6,5 +6,12 @@ class MoneyAccountForm(forms.ModelForm):
     class Meta:
         model = MoneyAccount
         fields = ['name', 'balance', 'type', 'currency_code', 'description']
+        labels = {
+            'name': 'Nazwa',
+            'balance': 'Saldo',
+            'type': 'Typ',
+            'currency_code': 'Kod waluty',
+            'description': 'Opis',
+        }
 
     description = forms.CharField(widget=forms.Textarea, required=False)
