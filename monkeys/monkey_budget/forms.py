9c471f7e6509fe5c7877622680a91e970bf90177
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-02-21 16:28:30 +0100

    description field has been set as nonrequired

--- monkeys/monkey_budget/forms.py
+++ monkeys/monkey_budget/forms.py
@@ -8,27 +8,29 @@ class MoneyAccountForm(forms.ModelForm):
         model = MoneyAccount
         fields = ['name', 'balance', 'type', 'currency_code', 'description']
 
-    name = forms.CharField(
-        max_length=100,
-        label="Nazwa konta",
-        widget=forms.TextInput(attrs={'placeholder': 'Podaj nazwę konta'})
-    )
-    balance = forms.IntegerField(
-        label="Początkowy stan konta (liczba całkowita)",
-        widget=forms.NumberInput()
-    )
-    type = forms.ChoiceField(
-        choices=MoneyAccount.types,
-        label="Typ konta",
-        initial='BA'
-    )
-    currency_code = forms.ChoiceField(
-        choices=CurrencyHelper.get_currencies_set(),
-        label="Waluta",
-        initial='PLN'
-    )
-    description = forms.CharField(
-        max_length=512,
-        label="Opis",
-        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Podaj krótki opis konta'})
-    )
+    description = forms.CharField(widget=forms.Textarea, required=False)
+
+    # name = forms.CharField(
+    #     max_length=100,
+    #     label="Nazwa konta",
+    #     widget=forms.TextInput(attrs={'placeholder': 'Podaj nazwę konta'})
+    # )
+    # balance = forms.IntegerField(
+    #     label="Początkowy stan konta (liczba całkowita)",
+    #     widget=forms.NumberInput()
+    # )
+    # type = forms.ChoiceField(
+    #     choices=MoneyAccount.types,
+    #     label="Typ konta",
+    #     initial='BA'
+    # )
+    # currency_code = forms.ChoiceField(
+    #     choices=CurrencyHelper.get_currencies_set(),
+    #     label="Waluta",
+    #     initial='PLN'
+    # )
+    # description = forms.CharField(
+    #     max_length=512,
+    #     label="Opis",
+    #     widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Podaj krótki opis konta'})
+    # )
