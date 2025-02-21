5b08420205bf91fc8e31f13e8baf3e907299c082
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-02-21 21:07:39 +0100

    Currency code display bug has been handled

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -21,7 +21,7 @@ class MoneyAccount(models.Model):
     description = models.TextField(max_length=512)
 
     def __str__(self):
-        return f'{self.name}: {self.show_balance()} {CurrencyHelper.get_currency_by_its_code(self.currency_code)["code"]}'
+        return f'{self.name}: {self.show_balance()}'
 
     def show_balance(self):
         money = Monetary(int(self.balance), CurrencyHelper.get_currency_by_its_code(self.currency_code))
