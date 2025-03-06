5dd6094f35625de5eeffd0cd91d56882485a5d0e
marekwocka <marekwocka@gmail.com>
2025-03-06 01:32:46 +0100

    Format part of code

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -55,7 +55,12 @@ class Transaction(models.Model):
     #  związanych z gwarancją)
     account = models.ForeignKey(MoneyAccount, on_delete=models.deletion.CASCADE, related_name='transaction')
     date = models.DateTimeField(default=django.utils.timezone.now)
-    total = models.BigIntegerField(validators=[MinValueValidator(limit_value=0, message='Transaction total value must be nonnegative')])
+    total = models.BigIntegerField(
+        validators=[MinValueValidator(
+            limit_value=0,
+            message='Transaction total value must be nonnegative')],
+        default=0,
+        editable=False)
     transaction_directions = [('IN', 'income'), ('OUT', 'outcome')]
     transaction_direction = models.CharField(choices=transaction_directions, max_length=3, default='OUT')
     balance_after_transaction = models.BigIntegerField()
