864c06443e55193aef18241757130d6a29f4a43c
Roman Szubryt <roman@szubryt.eu>
2025-03-16 19:52:24 +0100

    Add categories and subcategories JPYDZR6MON-54  (#27)
    * add categories & subcategories to admin
    
    * add categories & subcategories to data base model
    
    * migration to category & sub

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -40,6 +40,41 @@ class MoneyAccount(models.Model):
         verbose_name = 'Money Account'
 
 
+class MainCategory(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    user_id = models.ForeignKey(User, related_name='main_categories', on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=100)
+    category_types = [
+        ('INC', 'Przychód'),
+        ('EXP', 'Wydatek'),
+    ]
+    category_type = models.CharField(choices=category_types, max_length=3)
+    description = models.TextField(max_length=512, blank=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.get_category_type_display()})'
+
+    class Meta:
+        app_label = 'monkey_budget'
+        verbose_name = 'Main Category'
+        verbose_name_plural = 'Main Categories'
+
+
+class SubCategory(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
+    parent_category = models.ForeignKey(MainCategory, related_name='subcategories', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=512, blank=True)
+
+    def __str__(self):
+        return f'{self.parent_category.name} - {self.name}'
+
+    class Meta:
+        app_label = 'monkey_budget'
+        verbose_name = 'Sub Category'
+        verbose_name_plural = 'Sub Categories'
+
+
 class Transaction(models.Model):
     # TODO: Jeżeli konto miałoby zostać kiedyś usunięte to w przypadku, gdy posiada transakcje, być może powinno one
     #  zostać zarchiwizowane (dla raportów, w zależności jak będa one generowane lub dla zachowania faktur/paragonów
