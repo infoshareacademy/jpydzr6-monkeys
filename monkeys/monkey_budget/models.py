2ce824354576cb34202d2bc032eb329f5982f739
Dorota Grześkowiak <dorota@grzeskowiak.pro>
2025-03-19 12:09:43 +0100

    Add saving new_balance to account

--- monkeys/monkey_budget/models.py
+++ monkeys/monkey_budget/models.py
@@ -37,11 +37,12 @@ class MoneyAccount(models.Model):
     def get_type_display_name(self):
         return dict(self.types).get(self.type, self.type)
 
-    def validate_new_balance(self, new_balance: int) -> int:
+    def validate_new_balance(self, new_balance: int) -> None:
         if self.possibly_negative and new_balance < 0:
             raise ValidationError('Wybrane konto nie może posiadać ujemnego salda.')
         else:
             self.balance = new_balance
+            self.save()
 
 
     class Meta:
