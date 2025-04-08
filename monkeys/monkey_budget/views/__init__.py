e7a37b4a364e89d471ce9e8ebf5282658711c89e
marekwocka <marekwocka@gmail.com>
2025-04-08 22:34:29 +0200

    Update imports

--- monkeys/monkey_budget/views/__init__.py
+++ monkeys/monkey_budget/views/__init__.py
@@ -1,2 +1,3 @@
-from .account import *
+from .base import home, team, contact
+from .account import dashboard, show_money_account, add_money_account, edit_money_account, delete_money_account
 from .transaction import *
