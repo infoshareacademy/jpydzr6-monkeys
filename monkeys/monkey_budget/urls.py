cc5aa4523acd4dcbd3dfc1ffb6a80588fbbcacc5
Roman Szubryt <roman@szubryt.eu>
2025-04-11 21:38:07 +0200

    JPYDZR6MON-63 (#54)
    * user account templates & emails
    
    * user account
    
    * media for profile avatar
    
    * Fix:  Refactored template links to use namespaced
    
    * delete
    
    * delete double base
    
    * change reference to base template
    
    * addopt user profile in new templates
    
    * fix in view
    
    * fix dbmigrations
    
    * fix delete all old migrations, and create new one
    
    * fix: issues with users, translate

--- monkeys/monkey_budget/urls.py
+++ monkeys/monkey_budget/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'monkey_budget'
@@ -16,4 +17,25 @@ urlpatterns = [
     path('transakcje/nowa/', views.transaction_create_view, name='nowa-transakcja'),
     path('transakcje/edytuj/<int:transaction_id>', views.transaction_update_view, name='edytuj-transakcje'),
     path('transakcje/lista/', views.transaction_list, name='lista-transakcji'),
+    path('users/login/', views.CustomLoginView.as_view(), name='login'),
+    path('users/logout/', auth_views.LogoutView.as_view(
+        next_page='monkey_budget:home',
+        template_name='users/logout.html',
+    ), name='logout'),
+    path('users/register/', views.register, name='register'),
+    path('users/profile/', views.profile, name='profile'),
+    path('users/password/', auth_views.PasswordChangeView.as_view(
+        template_name='users/password_change.html',
+        success_url=reverse_lazy('monkey_budget:password_change_done')), 
+        name='password_change'),
+    path('users/password/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='users/password_change_done.html'), 
+        name='password_change_done'),
+    path('users/activate/<uidb64>/<token>/', views.activate_account, name='activate'),
+    path('users/password-reset/', views.password_reset_request, name='password_reset'),
+    path('users/password-reset/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
+    path('users/magic-link/', views.magic_link_request, name='magic_link_request'),
+    path('users/magic-login/<uidb64>/<token>/', views.magic_link_login, name='magic_link_login'),
+    path('users/delete-account/', views.delete_account, name='delete_account'),
+    path('users/resend-activation/', views.resend_activation_email, name='resend_activation'),
 ]
