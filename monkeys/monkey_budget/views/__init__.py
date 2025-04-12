1d47b895d5882563418f8cc2e42b59f3cfd432b9
marekwocka <marekwocka@gmail.com>
2025-04-12 10:50:19 +0200

    Fix trnsactions view init file

--- monkeys/monkey_budget/views/__init__.py
+++ monkeys/monkey_budget/views/__init__.py
@@ -1,5 +1,6 @@
 from .base import home, team, contact
 from .account import dashboard, show_money_account, add_money_account, edit_money_account, delete_money_account
 from .transaction import (TransactionCreateView, TransactionUpdateView, TransactionDeleteView,
-                          TransactionDetailView, TransactionListView)
-from .transaction import *
+                          TransactionDetailView, TransactionListView,
+                          AccountTransactionListView,
+                          get_account_currency_info)
